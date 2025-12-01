be6b11f9308f9d97ad8123581cf757a57fc72941
Imosemi Ehioken Jeremiah <jay@Imosemis-MacBook-Pro.local>
2025-12-02 02:46:40 +0300

    staticfiles

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nexusjobs.onrender.com']
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
